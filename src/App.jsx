3065a0798e1bbbc14cc3fda7e578d407a447abce
Rohan <shingaderohan96@gmail.com>
2024-06-02 17:57:10 +0530

    default styles commented

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Header from './components/common/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       <div>
-        <h1> Mooncart </h1>
+        <Header/>
       </div>
     </>
   )
