b88722fae2c82f728cf77562648641a5852d96c7
Rohan <shingaderohan96@gmail.com>
2024-06-04 20:21:41 +0530

    header component in making

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../../assets/common/logo.png'
 import { menulists } from '../../assets/data/data'
-import { CustomLink, Badges } from './CustomComponent'
+import { CustomNavLink, CustomLink, Badges } from './CustomComponent'
 import { IoSearchOutline, IoCartOutline } from "react-icons/io5";
 
 const Header = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  const menuRef = useRef(null);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  }
+
+  //close the menu if click outside of the menu
+  const closeMenuOutside = (event) => {
+    if(menuRef.current && !menuRef.current.contains(event.target)) {
+      setIsOpen(false)
+    }
+  }
+
   return (
     
       <header className='header px-12 py-3 bg-gray-200 relative z-20'>
@@ -16,34 +33,41 @@ const Header = () => {
         <nav className='p-4 flex justify-between items-center'>
           
           <div className='flex items-center gap-14'>
+
             <div>
               <img src={logo} alt='Mooncart Logo' className='h-7'/>
             </div>
+
             <div className='hidden lg:flex items-center justify-between gap-8'>
               {menulists.map((list)=>(
                 <li key={list.id} className='uppercase list-none'>
-                  <a href={list.path}> {list.link} </a>
+                  <CustomNavLink href={list.path}> {list.link} </CustomNavLink>
                 </li>
               ))}
             </div>
+
           </div>
 
           <div className='flex items-center gap-8 icons'>
+
             <div className='uppercase hidden lg:block text-inherit relative z-20'>
-              <CustomLink href='/' className=""> Login </CustomLink>
+              <CustomLink> Login </CustomLink>
               <span>/</span>
-              <CustomLink href='/' className=""> Register </CustomLink>
+              <CustomLink> Register </CustomLink>
             </div>
 
             <div className='icons flex items-center justify-center gap-6'>
               <IoSearchOutline size={23} />
+
               <div className='relative z-20'>
                 <IoCartOutline size={23} />
                 <div className='absolute -top-2 -right-1.5'> 
                   <Badges color="bg-red-600"> 0 </Badges>
                 </div>
               </div>
+
             </div>
+
           </div>
         </nav>
 
