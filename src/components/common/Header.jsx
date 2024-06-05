4918cf60b60dce224b3d4fe6e8888ac7cee5e0ef
Rohan <shingaderohan96@gmail.com>
2024-06-05 19:36:36 +0530

    some styling added to header

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,15 +1,16 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import logo from '../../assets/common/logo.png'
 import { menulists } from '../../assets/data/data'
 import { CustomNavLink, CustomLink, Badges } from './CustomComponent'
 import { IoSearchOutline, IoCartOutline } from "react-icons/io5";
+
 
 const Header = () => {
 
   const [isOpen, setIsOpen] = useState(false);
   const [isScrolled, setIsScrolled] = useState(false);
 
-  const menuRef = useRef(null);
+  // const menuRef = useRef(null);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
@@ -22,13 +23,37 @@ const Header = () => {
     }
   }
 
+  //handle scroll menu
+  const handleScroll = () => {
+    setIsScrolled(window.scrollY > 0);
+  }
+
+  useEffect(()=>{
+    document.addEventListener("mousedown", closeMenuOutside);
+    window.addEventListener("scroll", handleScroll);
+
+    return() => {
+      document.removeEventListener("mousedown", closeMenuOutside);
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
+  
+  const isHomePage = location.pathname === '/';
+
   return (
     
-      <header className='header px-12 py-3 bg-gray-200 relative z-20'>
+      <header className={isHomePage ? `header px-12 py-3 bg-gray-200 relative z-20 ${isScrolled ? "scrolled" : ""} ` : `header px-12 py-3 relative z-20 ${isScrolled ? "scrolled" : ""}`}>
 
-        {/* <div className='lg:h-[88px] lg:absolute lg:top-0 lg:right-0 lg:w-1/3 lg:z-10'>
+        {isHomePage && (
 
-        </div> */}
+          <div className={`${isScrolled ? "lg:bg-none" : "lg:bg-black"} lg:h-[88px] lg:absolute lg:top-0 lg:right-0 lg:w-1/3 lg:z-10`}>
+
+          </div>
+
+        // lg:h-[88px] lg:absolute lg:top-0 lg:right-0 lg:w-1/3 lg:z-10
+        )}
+
 
         <nav className='p-4 flex justify-between items-center'>
           
@@ -50,25 +75,33 @@ const Header = () => {
 
           <div className='flex items-center gap-8 icons'>
 
-            <div className='uppercase hidden lg:block text-inherit relative z-20'>
-              <CustomLink> Login </CustomLink>
-              <span>/</span>
-              <CustomLink> Register </CustomLink>
+            <div className='uppercase hidden lg:block relative z-20'>
+
+              <CustomLink className={`${isScrolled || isHomePage ? "text-white" : "text-green-600"}`}> Login </CustomLink>
+              <span className={`${isScrolled || isHomePage ? "text-white" : "text-green-600"}`}>/</span>
+              <CustomLink className={`${isScrolled || isHomePage ? "text-white" : "text-green-600"}`}> Register </CustomLink>
+              
             </div>
 
-            <div className='icons flex items-center justify-center gap-6'>
-              <IoSearchOutline size={23} />
+            <div className={`icons flex items-center justify-center gap-6 ${isScrolled || isHomePage ? "text-white" : "text-green-600"}`}>
+              <IoSearchOutline size={23} className={`${isScrolled || isHomePage ? "text-white" : "text-green-600"}`} />
 
               <div className='relative z-20'>
-                <IoCartOutline size={23} />
+                <IoCartOutline size={23} className={`${isScrolled || isHomePage ? "text-white" : "text-green-600"}`} />
                 <div className='absolute -top-2 -right-1.5'> 
-                  <Badges color="bg-red-600"> 0 </Badges>
+                  <Badges color="bg-red-600">0</Badges>
+                </div>
+              </div>
+              
+              <div className='relative z-20'>
+                <IoCartOutline size={23} className={`${isScrolled || isHomePage ? "text-white" : "text-green-600"}`} />
+                <div className='absolute -top-2 -right-1.5'> 
+                  <Badges color="bg-red-600">0</Badges>
                 </div>
               </div>
 
             </div>
-
-          </div>
+          </div> 
         </nav>
 
       </header>
