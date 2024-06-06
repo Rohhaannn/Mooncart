a33b7e9272a47a4020b873cb9667d363121b25e2
Rohan <shingaderohan96@gmail.com>
2024-06-07 00:59:46 +0530

    small screen menu error resolved

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,134 +1,105 @@
-import React, { useEffect, useRef, useState } from 'react'
-import logo from '../../assets/common/logo.png'
-import { menulists } from '../../assets/data/data'
-import { CustomNavLink, CustomLink, Badges } from './CustomComponent'
-import { IoSearchOutline, IoCartOutline, IoHeart, IoHeartOutline } from "react-icons/io5";
+import React, { useEffect, useRef, useState } from 'react';
+import logo from '../../assets/common/logo.png';
+import { menulists } from '../../assets/data/data';
+import { CustomNavLink, CustomLink, Badges } from './CustomComponent';
+import { IoSearchOutline, IoCartOutline, IoHeartOutline } from "react-icons/io5";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import { useLocation } from 'react-router-dom';
 
-
 const Header = () => {
-
   const [isOpen, setIsOpen] = useState(false);
   const [isScrolled, setIsScrolled] = useState(false);
   const menuRef = useRef(null);
-  const location = useLocation(null);
+  const location = useLocation();
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
-  }
+  };
 
-  //close the menu if click outside of the menu
+  // Close the menu if click outside of the menu
   const closeMenuOutside = (event) => {
-    if(menuRef.current && !menuRef.current.contains(event.target)) {
-      setIsOpen(false)
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setIsOpen(false);
     }
-  }
+  };
 
-  //handle scroll menu
+  // Handle scroll menu
   const handleScroll = () => {
     setIsScrolled(window.scrollY > 0);
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     document.addEventListener("mousedown", closeMenuOutside);
     window.addEventListener("scroll", handleScroll);
 
-    return() => {
+    return () => {
       document.removeEventListener("mousedown", closeMenuOutside);
       window.removeEventListener("scroll", handleScroll);
     };
-  });
+  }, []);
 
-  
   const isHomePage = location.pathname === '/';
 
   return (
-    
-      <header className={isHomePage ? `header px-12 py-3 bg-gray-200 relative z-20 ${isScrolled ? "scrolled" : ""} ` : `header px-12 py-3 relative z-20 ${isScrolled ? "scrolled" : ""}`}>
+    <header className={isHomePage ? `header px-12 py-3 bg-gray-200 relative z-20 ${isScrolled ? "scrolled" : ""}` : `header px-12 py-3 relative z-20 ${isScrolled ? "scrolled" : ""}`}>
+      {isHomePage && (
+        <div className={`${isScrolled ? "lg:bg-none" : "lg:bg-black"} lg:h-[88px] lg:absolute lg:top-0 lg:right-0 lg:w-1/3 lg:z-10`}></div>
+      )}
 
-        {isHomePage && (
-
-          <div className={`${isScrolled ? "lg:bg-none" : "lg:bg-black"} lg:h-[88px] lg:absolute lg:top-0 lg:right-0 lg:w-1/3 lg:z-10`}>
-
+      <nav className='p-4 flex justify-between items-center'>
+        <div className='flex items-center gap-14'>
+          <div>
+            <img src={logo} alt='Mooncart Logo' className='h-7' />
           </div>
-
-        // lg:h-[88px] lg:absolute lg:top-0 lg:right-0 lg:w-1/3 lg:z-10
-        )}
-
-
-        <nav className='p-4 flex justify-between items-center'>
-          
-          <div className='flex items-center gap-14'>
-
-            <div>
-              <img src={logo} alt='Mooncart Logo' className='h-7'/>
-            </div>
-
-            <div className='hidden lg:flex items-center justify-between gap-8'>
-              {menulists.map((list)=>(
-                <li key={list.id} className='uppercase list-none'>
-                  <CustomNavLink href={list.path}> {list.link} </CustomNavLink>
-                </li>
-              ))}
-            </div>
-
-          </div>
-
-          <div className='flex items-center gap-8 icons'>
-            <div className='uppercase hidden lg:block text-inherit relative z-20'>
-
-              <CustomLink className={`${isScrolled || !isHomePage ? "text-black" : "text-white"}`}> Login </CustomLink>
-              <span className={`${isScrolled || !isHomePage ? "text-black" : "text-white"}`}>/</span>
-              <CustomLink className={`${isScrolled || !isHomePage ? "text-black" : "text-white"}`}> Register </CustomLink>
-              
-            </div>
-
-            <div className={`icons flex items-center justify-center gap-6 cursor-pointer ${isScrolled || !isHomePage ? "text-black" : "text-white"}`}>
-              <div className='relative z-20'>
-                <IoSearchOutline size={23} className={`${isScrolled || !isHomePage ? "text-black" : "text-white"}`} />
-              </div>
-
-              <div className='relative z-20'>
-                <IoHeartOutline size={23} className={`${isScrolled || !isHomePage ? "text-black" : "text-white"}`} />
-                <div className='absolute -top-2 -right-1.5'> 
-                  <Badges color="bg-green-600">0</Badges>
-                </div>
-              </div>
-
-              <div className='relative z-20'>
-                <IoCartOutline size={23} className={`${isScrolled || !isHomePage ? "text-black" : "text-white"}`} />
-                <div className='absolute -top-2 -right-1.5'> 
-                  <Badges color="bg-red-600">0</Badges>
-                </div>
-              </div>
-
-              <button 
-                onClick={toggleMenu}
-                className='lg:hidden w-10 h-10 flex justify-center items-center bg-black text-white focus:outline-none'
-              >
-                {isOpen ? <AiOutlineClose size={24}/> : <AiOutlineMenu size={24}/>}
-              </button>
-
-            </div>
-          </div> 
-          
-
-          {/* Responsive menu if below the 768px */}
-
-          <div ref={menuRef} className={`lg:flex lg:items-center lg:w-auto w-full p-5 absolute right-0 top-0 menu-container ${isOpen} ? "open" : "closed"`}>
-            {menulists.map((list)=>(
+          <div className='hidden lg:flex items-center justify-between gap-8'>
+            {menulists.map((list) => (
               <li key={list.id} className='uppercase list-none'>
-                <CustomNavLink href={list.path} className='text-white'> {list.link} </CustomNavLink>
+                <CustomNavLink href={list.path}>{list.link}</CustomNavLink>
               </li>
             ))}
           </div>
+        </div>
+        <div className='flex items-center gap-8 icons'>
+          <div className='uppercase hidden lg:block text-inherit relative z-20'>
+            <CustomLink className={`${isScrolled || !isHomePage ? "text-black" : "text-white"}`}>Login</CustomLink>
+            <span className={`${isScrolled || !isHomePage ? "text-black" : "text-white"}`}>/</span>
+            <CustomLink className={`${isScrolled || !isHomePage ? "text-black" : "text-white"}`}>Register</CustomLink>
+          </div>
+          <div className={`icons flex items-center justify-center gap-6 cursor-pointer ${isScrolled || !isHomePage ? "text-black" : "text-white"}`}>
+            <div className='relative z-20'>
+              <IoSearchOutline size={23} className={`${isScrolled || !isHomePage ? "text-black" : "text-white"}`} />
+            </div>
+            <div className='relative z-20'>
+              <IoHeartOutline size={23} className={`${isScrolled || !isHomePage ? "text-black" : "text-white"}`} />
+              <div className='absolute -top-2 -right-1.5'>
+                <Badges color="bg-green-600">0</Badges>
+              </div>
+            </div>
+            <div className='relative z-20'>
+              <IoCartOutline size={23} className={`${isScrolled || !isHomePage ? "text-black" : "text-white"}`} />
+              <div className='absolute -top-2 -right-1.5'>
+                <Badges color="bg-red-600">0</Badges>
+              </div>
+            </div>
+            <button
+              onClick={toggleMenu}
+              className='lg:hidden w-10 h-10 flex justify-center items-center bg-black text-white focus:outline-none'
+            >
+              {isOpen ? <AiOutlineClose size={24} /> : <AiOutlineMenu size={24} />}
+            </button>
+          </div>
+        </div>
+        {/* Responsive menu if below the 768px */}
+        <div ref={menuRef} className={`lg:hidden flex-col w-full p-5 absolute right-0 top-full transition-transform duration-300 transform ${isOpen ? 'open' : 'closed'} menu-container`}>
+          {menulists.map((list) => (
+            <li key={list.id} className='uppercase list-none'>
+              <CustomNavLink href={list.path} className='text-white'>{list.link}</CustomNavLink>
+            </li>
+          ))}
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-        </nav>
-
-      </header>
-    
-  )
-}
-
-export default Header
+export default Header;
