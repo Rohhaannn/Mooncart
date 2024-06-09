9d0608f5a152d7ea05d8c767db46723884c4ca41
Rohan <shingaderohan96@gmail.com>
2024-06-09 17:57:04 +0530

    footer commented from layout (temp)

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Header from './Header'
-import Footer from './Footer'
+// import Footer from './Footer'
 import PropTypes from "prop-types";
 
 const Layout = ({ children }) => {
   return (
     <div>
       <Header/>
-      <main style={{minHeight: "80vh"}}> {children} </main>
-      <Footer/>
+      <main style={{minHeight: "90vh"}}> {children} </main>
+      {/* <Footer/> */}
     </div>
   )
 }
