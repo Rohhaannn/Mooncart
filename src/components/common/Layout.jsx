29f9614579a564d4a260326b9595fe5b1d271f38
Rohan <shingaderohan96@gmail.com>
2024-06-10 12:42:02 +0530

    footer component updated & rendered

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
-import Header from './Header'
-// import Footer from './Footer'
-import PropTypes from "prop-types";
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import PropTypes from 'prop-types';
 
 const Layout = ({ children }) => {
   return (
-    <div>
-      <Header/>
-      <main style={{minHeight: "90vh"}}> {children} </main>
-      {/* <Footer/> */}
+    // <div style={{ display: 'flex', flexDirection: 'column', minHeight: '215vh' }}>
+    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <Header />
+        <main style={{ flex: 1 }}>
+          {children}
+        </main>
+      <Footer />
     </div>
-  )
-}
-
-export default Layout;
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
+
+export default Layout;
