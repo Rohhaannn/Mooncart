e182b1871c6d6e4aefe96dbaf90c0138748ba99e
Rohan <shingaderohan96@gmail.com>
2024-07-02 19:15:35 +0530

    a temporary page was added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import { Layout, Home, Shop, ProductDetails, CartPage, Blog } from './router';
+import ComingSoon from './screen/ComingSoon';
 
 
 function App() {
@@ -10,6 +11,9 @@ function App() {
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
         <Route path='/blog' element={<Blog />} />
+        <Route path='/about' element={<ComingSoon />} />
+        <Route path='/services' element={<ComingSoon />} />
+        <Route path='/contact' element={<ComingSoon />} />
         <Route path='product-details/:productId' element={<ProductDetails />} />
         <Route path='/cart' element={<CartPage />} />
       </Route>
---
+++ src/screen/ComingSoon.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ComingSoon = () => {
+  return (
+    <div className='mt-36 text-[32px] text-center font-bold'>
+      <h1>Coming Soon</h1>
+      <p>This page is under development. Please check back later.</p>
+    </div>
+  )
+}
+
+export default ComingSoon
